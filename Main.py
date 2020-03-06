653d8d8a0f2b745d1939d8c4f12d8c5f2a5422a3
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-03-06 12:01:36 -0500

    Hago principalmente coger los datos y  las direccines de las imágenes, creo el modelo a partir de resnet y  lo copilo.

--- Main.py
+++ Main.py
@@ -2,6 +2,20 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from tensorflow.keras import layers
 from tensorflow import keras
+import cv2
+from sklearn.model_selection import train_test_split
+
+# TPU_WORKER = 'grpc://10.240.1.2:8470'
+#
+#
+# resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu=TPU_WORKER)
+# tf.config.experimental_connect_to_cluster(resolver)
+# tf.tpu.experimental.initialize_tpu_system(resolver)
+# strategy = tf.distribute.experimental.TPUStrategy(resolver)
+#
+
+
+
 import pandas as pd
 images_path ="D:\miia4406-movie-genre-classification\images"
 df_train =open("D:\miia4406-movie-genre-classification/dataTraining.csv").readlines()
@@ -20,6 +34,22 @@ def transform_labels(string):
         label.append(set_labels.index(thing.strip()))
     return label
 import numpy as np
+def create_batch(X,Y,batch_size):
+    sub_x=np.zeros((500,224,224,3))
+    sub_y=np.zeros((500,24))
+    indices=np.random.choice(range(len(Y)),batch_size)
+    for i in indices:
+        im =cv2.imread(X[i].replace("\"","")+".jpeg")
+        im=cv2.resize(im,(224,224))
+        sub_x[i,:,:,:]=im
+        temp = np.zeros(1,24)
+        temp[Y[i]]=1
+        sub_y[i,:]=temp
+
+
+    return tf.constant(sub_x,dtype=tf.float32),tf.constant(sub_y,dtype=tf.float32)
+
+
 
 def give_label(string):
     cos = string.partition("[")[2].partition("]")[0]
@@ -36,6 +66,11 @@ labels =list(map(transform_labels,labels))
 # for i ,lab in enumerate(set_labels):
 #     dict_labels[lab]=i
 ids = list(map(add_prefix,list(map(give_id,df_train))))
+X_train,X_test,y_train,y_test=train_test_split(ids,labels,test_size=0.1)
+BATCH_SIZE=500
+
+
+
 
 
 
@@ -48,9 +83,20 @@ feature_extractor_layer.trainable = False
 model = tf.keras.Sequential([
   feature_extractor_layer,
   layers.Dense(len(set_labels), activation=keras.activations.hard_sigmoid,dtype=tf.float32)
-])
+],name="Modelo_peliculas")
 optim=keras.optimizers.Adam(learning_rate=0.0001)
-metrics=
-model.compile(optimizer=optim,loss=tf.keras.metrics.CategoricalAccuracy,)
+metrics=[keras.metrics.CategoricalAccuracy(name="Categorical_accuracy"),
+        keras.metrics.TruePositives(name='tp'),
+      keras.metrics.FalsePositives(name='fp'),
+      keras.metrics.TrueNegatives(name='tn'),
+      keras.metrics.FalseNegatives(name='fn')]
+model.compile(optimizer=optim,loss=tf.keras.metrics.CategoricalAccuracy,metrics=metrics)
 
+for i in range(10):
+    x,y =create_batch(X_train,y_train,BATCH_SIZE)
+    x_test,y_test_mini = create_batch(X_test,y_test,BATCH_SIZE)
+    model.fit(x=x,y=y,epochs=10,validation_data=[x_test,y_test_mini])
+
+
+model.save("modelo.h5")
 pass
