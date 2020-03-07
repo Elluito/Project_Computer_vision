c98a9321e4d303203d96e35de729f9eb91cb7f21
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-03-07 17:46:22 -0500

    Pueden poner a correr este modelo.

--- Main.py
+++ Main.py
@@ -15,8 +15,7 @@ from sklearn.model_selection import train_test_split
 #
 
 
-
-import pandas as pd
+# Esto se teiene que cambiar a la ruta donde esten las imágenes y el archivo CSV del dataset
 images_path ="D:\miia4406-movie-genre-classification\images"
 df_train =open("D:\miia4406-movie-genre-classification/dataTraining.csv").readlines()
 # df_test =open("D:\miia4406-movie-genre-classification/dataTesting.csv").readlines()
@@ -34,21 +33,26 @@ def transform_labels(string):
         label.append(set_labels.index(thing.strip()))
     return label
 import numpy as np
-def create_batch(X,Y,batch_size):
-    sub_x=np.zeros((500,224,224,3))
-    sub_y=np.zeros((500,24))
+
+def create_batch(X,Y,batch_size,tupl=False):
+    sub_x=np.zeros((batch_size,224,224,3))
+    sub_y=np.zeros((batch_size,24))
     indices=np.random.choice(range(len(Y)),batch_size)
+    j=0
     for i in indices:
         im =cv2.imread(X[i].replace("\"","")+".jpeg")
         im=cv2.resize(im,(224,224))
-        sub_x[i,:,:,:]=im
-        temp = np.zeros(1,24)
+        sub_x[j,:,:,:]=im
+        temp = np.zeros(24)
         temp[Y[i]]=1
-        sub_y[i,:]=temp
+        sub_y[j,:]=temp
+        j+=1
 
-
-    return tf.constant(sub_x,dtype=tf.float32),tf.constant(sub_y,dtype=tf.float32)
-
+    data= tf.data.Dataset.from_tensor_slices((tf.constant(sub_x, dtype=tf.float32), tf.constant(sub_y, dtype=tf.float32))).batch(10)
+    tupla=tf.constant(sub_x,dtype=tf.float32),tf.constant(sub_y,dtype=tf.float32)
+    if not tupl:
+        return data
+    return sub_x,sub_y
 
 
 def give_label(string):
@@ -67,13 +71,34 @@ labels =list(map(transform_labels,labels))
 #     dict_labels[lab]=i
 ids = list(map(add_prefix,list(map(give_id,df_train))))
 X_train,X_test,y_train,y_test=train_test_split(ids,labels,test_size=0.1)
+
+
+import pickle
+with open("x_train","w+b") as fp:
+    pickle.dump(X_train,fp)
+with open("x_test","w+b") as fp:
+    pickle.dump(X_test,fp)
+with open("y_train","w+b") as fp:
+    pickle.dump(y_train,fp)
+with open("y_test", "w+b") as fp:
+    pickle.dump(y_test,fp)
+
+
+
 BATCH_SIZE=500
 
-
-
-
-
-
+# X_train =None
+# X_test =None
+# y_train =None
+# y_test =None
+# with open("x_train","r+b") as fp:
+#     X_train=pickle.load(fp)
+# with open("x_test","r+b") as fp:
+#     X_test=pickle.load(fp)
+# with open("y_train","r+b") as fp:
+#      y_train=pickle.load(fp)
+# with open("y_test", "r+b") as fp:
+#     y_test=pickle.load(fp)
 feature_extractor_url="https://tfhub.dev/google/imagenet/resnet_v1_101/feature_vector/4"
 
 
@@ -82,7 +107,7 @@ feature_extractor_layer.trainable = False
 
 model = tf.keras.Sequential([
   feature_extractor_layer,
-  layers.Dense(len(set_labels), activation=keras.activations.hard_sigmoid,dtype=tf.float32)
+  layers.Dense(24, activation=keras.activations.hard_sigmoid,dtype=tf.float32)
 ],name="Modelo_peliculas")
 optim=keras.optimizers.Adam(learning_rate=0.0001)
 metrics=[keras.metrics.CategoricalAccuracy(name="Categorical_accuracy"),
@@ -90,12 +115,15 @@ metrics=[keras.metrics.CategoricalAccuracy(name="Categorical_accuracy"),
       keras.metrics.FalsePositives(name='fp'),
       keras.metrics.TrueNegatives(name='tn'),
       keras.metrics.FalseNegatives(name='fn')]
-model.compile(optimizer=optim,loss=tf.keras.metrics.CategoricalAccuracy,metrics=metrics)
+loss=tf.keras.losses.CategoricalCrossentropy()
+model.compile(optimizer=optim,loss=loss,metrics=metrics)
+with tf.device("GPU:0"):
+    for i in range(10):
+        x,y=create_batch(X_train,y_train,BATCH_SIZE,tupl=True)
+        x_test,Y_test= create_batch(X_test,y_test,BATCH_SIZE,tupl=True)
 
-for i in range(10):
-    x,y =create_batch(X_train,y_train,BATCH_SIZE)
-    x_test,y_test_mini = create_batch(X_test,y_test,BATCH_SIZE)
-    model.fit(x=x,y=y,epochs=10,validation_data=[x_test,y_test_mini])
+        model.fit(x,y,batch_size=50,epoch=10,validation_data=(x_test,Y_test),validation_steps=10)
+        # model.evaluate(x_test,Y_test)
 
 
 model.save("modelo.h5")
