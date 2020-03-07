d4f900ee702e5c7b98424569c6930446398ac127
LuisAlfredo <alfredoavendano@hotrmail.com>
2020-03-07 17:58:46 -0500

    Pueden poner a correr este modelo.

--- Main.py
+++ Main.py
@@ -35,6 +35,25 @@ def give_label(string):
     for i in cos1:
         set_labels.add(i.strip())
     return cos.strip()
+
+def input_fn(batch_size=16):
+    """An input_fn to parse 28x28 images from filename using tf.data."""
+    # batch_size = params["batch_size"]
+    # with tf.io.gfile.GFile(PATH_TO_BATCH, 'r+b') as f:
+    #     txt = f.read()
+    # source=tf.constant(txt, dtype=tf.float32)
+    # print(source)
+    with open(PATH_TO_BATCH,"r+b") as fp:
+        state_batch,q_values = pickle.load(fp)
+    state_batch =tf.constant(state_batch,dtype=tf.float32)
+    q_values = tf.constant(q_values,dtype=tf.float32)
+
+    prob_dataset = tf.data.Dataset.from_tensor_slices((state_batch,q_values))
+
+    batchd_prob = prob_dataset.batch(batch_size, drop_remainder=True)
+    # batchd_prob =batchd_prob.cache()
+    return batchd_prob.repeat()
+
 del df_train[0]
 labels =list(map(give_label,df_train))
 set_labels =list(set_labels)
@@ -56,8 +75,15 @@ with strategy.scope():
                 feature_extractor_layer,
                 layers.Dense(len(set_labels), activation=keras.activations.hard_sigmoid,dtype=tf.float32)])
 
-    optim=keras.optimizers.Adam(learning_rate=0.0001)
-metrics=
-model.compile(optimizer=optim,loss=tf.keras.metrics.CategoricalAccuracy,)
+    optim=keras.optimizers.Adam(learning_rate=0.001)
+    metrics=[keras.metrics.CategoricalAccuracy(name="Categorical_accuracy"),
+        keras.metrics.TruePositives(name='tp'),
+      keras.metrics.FalsePositives(name='fp'),
+      keras.metrics.TrueNegatives(name='tn'),
+      keras.metrics.FalseNegatives(name='fn')]
+    loss=tf.keras.losses.CategoricalCrossentropy()
+    model.compile(optimizer=optim,loss=loss,metrics=metrics)
+self.model.fit(input_fn(),steps_per_epoch=20,epochs=1,verbose=0)
+
 
 pass
